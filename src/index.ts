c4069833cae18fc0620b7dfbb8c89481a5a09571
Nils Martel <nilsmartel@yahoo.de>
2022-05-06 12:02:44 +0200

    Make animations consistent

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,69 @@
 type Action = (factor: number) => Promise<void>
 
-const elementToWatch: [HTMLElement | string, Action][] = []
+
+/**
+ * Collection of all Elements we watch for our animation
+ * alongside state to remember, whether we have already started our animations or not
+ */
+const elementToWatch: [HTMLElement | string, Action, AnimationState | null][] = []
+
+enum AnimationState {
+  Start,
+  Between,
+  End,
+}
 
 const updateScrollAnimations = async function () {
-  let pageScroll = window.pageYOffset || document.documentElement.scrollTop
+  for (let index in elementToWatch) {
+    let [element, action, state] = elementToWatch[index]
+    const setState = (state: AnimationState) => {
+      elementToWatch[index][2] = state
+    }
 
-  for (let [element, action] of elementToWatch) {
     if (typeof element === "string") {
       const maybeElement = document.getElementById(element)
       if (!maybeElement) continue
       element = maybeElement
     }
 
-    const {height, y} = element.getBoundingClientRect()
+    const { height, y } = element.getBoundingClientRect()
 
-    const position = pageScroll - y
+    let factor: number = (-y / height)
+    if (isNaN(factor)) {
+      // height might be 0
+      // in that case we want to have abinary decision about being either in start or end animation state  
+      if (y > 0) factor = 0
+      else factor = 1
+    }
 
-    if (position >= 0 && position <= height) {
-      let factor: number = (position / height)
-      if (isNaN(factor)) factor = 1.0
-
+    // check if we're in between a running animation
+    if (factor > 0 && factor < 1) {
       action(factor)
+      setState(AnimationState.Between)
+      // nothing left to consider now
+      continue
+    }
+
+    // we want to run the animation at least once, to be consistent when it starts
+    // this is guaranteed by the state beeing null only on startup,
+    // and by some animation always happening in between, before and after an animation
+  
+    // we want to ensure that the animation is up to date before and after,
+    // as to not leave the screen with intermediate artifacts, when the exact 0 | 1 point has been reached
+
+    // check if we should be in start configuration (factor <= 0) but aren't
+    if (factor <= 0 && state !== AnimationState.Start) {
+      // if so, fire animation to make state consistent
+      action(0)
+      setState(AnimationState.Start)
+      continue
+    }
+
+    // check if the animation should have ran already (factor >= 1), but hasn't
+    if (factor >= 1 && state !== AnimationState.End) {
+      action(1)
+      setState(AnimationState.End)
+      continue
     }
   }
 }
@@ -29,5 +72,5 @@ window.onscroll = updateScrollAnimations
 window.onpageshow = updateScrollAnimations
 
 export default function animate(element: HTMLElement | string, action: Action) {
-  elementToWatch.push([element, action])
+  elementToWatch.push([element, action, null])
 }
